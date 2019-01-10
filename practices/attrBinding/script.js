5ce883a250b4dc71fd2429177a8ef8a58ff12fa2
Manny Jo <jomansang@gmail.com>
2019-01-10 17:37:04 -0600

    Add attribute binding example

---
+++ practices/attrBinding/script.js
@@ -0,0 +1,9 @@
+let app = new Vue({
+    el: '#app',
+    data: {
+        product: 'Woori Dottie',
+        altText: 'The Cutiest dog ever',
+        image: '../images/dottie.jpg',
+        link: 'http://www.mansangjo.com'
+    }
+});
